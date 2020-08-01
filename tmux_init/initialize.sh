52d6507c7e24798324643c16232af044088453b0
Steven Cagle <sjcagle1@gmail.com>
2020-07-31 20:29:38 -0700

    added tmux init edge case protection

--- tmux_init/initialize.sh
+++ tmux_init/initialize.sh
@@ -1,1 +1,6 @@
+# If initialized after already stowed, then a git-submodule is created, since it places
+# the tpm plugin directory in the sym-linked directory (inside repository) instead
+# of the filesystem (output repository). So I make sure to unstow first before init.
+(cd ..; stow stow && stow -D tmux_init)
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/tmux-tpm/plugins/tpm
+(cd ..; stow tmux_init)
