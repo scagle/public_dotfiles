e4d549a46fc58851e386a6e5a369a52a20f7f729
Steven Cagle <Steven.Cagle@wdc.com>
2020-08-07 17:33:46 -0700

    Souped up initialize.sh a bit

--- initialize.sh
+++ initialize.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-DOTCONFIG=$HOME/.config
-if [[ ! -d "$DOTCONFIG" ]]; then
-    mkdir "$DOTCONFIG"
-    echo "$DOTCONFIG created"
-fi
-echo "Linking stow configurations"
-stow --verbose -t ~ stow/
-
 common() {
     stow bash_init rg scripts shell stow tmux_init vim_plus zsh_init
     (cd vim_plus && stow nvim_sub vimwiki_sub)
@@ -27,7 +19,39 @@ linux_i3() {
     (cd _linux_plus && stow i3_sub)
 }
 
-[[ $1 == "common" ]] && common
-[[ $1 == "mac_common" ]] && mac_common
-[[ $1 == "linux_common" ]] && linux_common
-[[ $1 == "linux_i3" ]] && linux_i3
+print_usage() {
+    echo "Usage:"
+    echo "  Arg1: Configuration Type"
+    echo "    common       : sensible configurations for most systems"
+    echo "    mac_common   : common + Mac specific configurations"
+    echo "    linux_common : common + Linux specific configurations"
+    echo "    linux_i3     : common + linux_common + i3 specific configurations"
+}
+
+if [[ -z $1 ]] ; then
+    print_usage 
+    exit 1 
+fi
+
+DOTCONFIG=$HOME/.config
+if [[ ! -d "$DOTCONFIG" ]]; then
+    mkdir "$DOTCONFIG"
+    echo "$DOTCONFIG created"
+fi
+echo "Linking stow configurations"
+stow --verbose -t ~ stow/
+
+case $1 in
+    "common" )
+        common ;;
+    "mac_common" )
+        mac_common ;;
+    "linux_common" )
+        linux_common ;;
+    "linux_i3" )
+        linux_i3 ;;
+    * )
+        echo "Invalid argument '$1'"
+        print_usage
+        exit 1 ;; 
+esac
